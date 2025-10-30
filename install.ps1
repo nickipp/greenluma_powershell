0cbc515b0d2cf4677628a1ecd9f160a29fe0fd85
nickipp <crazy.fun3049@gmail.com>
2025-10-30 01:05:38 -0700

    added message to inform when not run as administrator

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 # Check if the script is running with administrator privileges
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host "This script requires administrator privileges. Please re-open script with administrator."
+    Read-Host -Prompt "Press enter to exit"
     # Start-Process powershell.exe -Verb RunAs -ArgumentList "-File", "`"$($MyInvocation.MyCommand.Path)`""
     # exit
 }
