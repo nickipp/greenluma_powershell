e095ffde4754f974953f43a26ab3920a60d310e1
nickipp <crazy.fun3049@gmail.com>
2025-10-30 01:21:04 -0700

    separated antivirus exclusion

---
+++ antivirus_exclusion.ps1
@@ -0,0 +1,18 @@
+# Check if the script is running with administrator privileges
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "This script requires administrator privileges. Please re-open script with administrator."
+    Read-Host -Prompt "Press enter to exit"
+}
+
+$check = Read-Host -Prompt "Are you sure you want to apply a Antivirus Exclusion to the folder? [Y/N] (Default = N)"
+$check = $check.ToLower()
+
+if ($check -ne "y"){
+    exit
+}
+
+Set-Location -Path $PSScriptRoot
+
+$temp = Get-Location | Select-Object -expand Path
+
+Add-MpPreference -ExclusionPath $temp
