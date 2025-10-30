5e3d29da2e326381d6927170e557c2a00881ab69
nickipp <crazy.fun3049@gmail.com>
2025-10-30 01:01:08 -0700

    testing print

--- install.ps1
+++ install.ps1
@@ -1,8 +1,8 @@
 # Check if the script is running with administrator privileges
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host "This script requires administrator privileges. Please re-open script with administrator."
-    Start-Process powershell.exe -Verb RunAs -ArgumentList "-File", "`"$($MyInvocation.MyCommand.Path)`""
-    exit
+    # Start-Process powershell.exe -Verb RunAs -ArgumentList "-File", "`"$($MyInvocation.MyCommand.Path)`""
+    # exit
 }
 
 $check = Read-Host -Prompt "Are you sure you want to install GreenLuma, and apply a Antivirus Exclusion to the folder? [Y/N] (Default = N)"
