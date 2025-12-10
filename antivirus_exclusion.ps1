c6155f344115d3df451ed025ea1bc48b97b7ae72
nickipp <crazy.fun3049@gmail.com>
2025-12-10 00:41:50 -0800

    updated to create directory if one does not exist and apply the exclusion

--- antivirus_exclusion.ps1
+++ antivirus_exclusion.ps1
@@ -2,17 +2,25 @@
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host "This script requires administrator privileges. Please re-open script with administrator."
     Read-Host -Prompt "Press enter to exit"
+    return
 }
 
+# user input check before running
 $check = Read-Host -Prompt "Are you sure you want to apply a Antivirus Exclusion to the folder? [Y/N] (Default = N)"
 $check = $check.ToLower()
 
 if ($check -ne "y"){
-    exit
+    return
 }
 
-Set-Location -Path $PSScriptRoot
+$local_appdata_location = $env:LOCALAPPDATA
+$install_location = $local_appdata_location + "\Programs\GreenLuma"
+$folderPath = "$install_location"
 
-$temp = Get-Location | Select-Object -expand Path
+# Check if the folder exists and create folder if not
+if (-Not (Test-Path -Path $folderPath)) {
+    New-Item -Path "$install_location" -ItemType Directory
+}
 
-Add-MpPreference -ExclusionPath $temp
+# adds exclusion to install path
+Add-MpPreference -ExclusionPath $folderPath
